48bb097c3b2dbb888f4d1d25397e292da8a7ba90
Ryan Ponte <ryan.ponte@gmail.com>
2021-07-21 15:57:09 -0400

    Setup Redux, add alert reducer and action

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -11,22 +16,24 @@ import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route
-            exact
-            path='/register'
-            component={Register}
-          />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route
+              exact
+              path='/register'
+              component={Register}
+            />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
