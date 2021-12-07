6a3767b81c84ab0277ac6d5f1657c9923981ce1b
Ryan Ponte <ryan.ponte@gmail.com>
2021-12-07 10:29:30 -0500

    Fix proxy issue, add Login functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,21 +1,26 @@
-import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
+import api from '../utils/api';
 import { setAlert } from './alert';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
 } from './types';
+
+/*
+  NOTE: we don't need a config object for axios as the
+ default headers in axios are already Content-Type: application/json
+ also axios stringifies and parses JSON for you, so no need for 
+ JSON.stringify or JSON.parse
+*/
 
 // Load User
 export const loadUser = () => async (dispatch) => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
-
   try {
-    const res = await axios.get('/api/auth');
+    const res = await api.get('/auth');
 
     dispatch({
       type: USER_LOADED,
@@ -29,28 +34,44 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register =
-  ({ name, email, password }) =>
-  async (dispatch) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
+export const register = (formData) => async (dispatch) => {
+  try {
+    const res = await api.post('/users', formData);
 
-    const body = JSON.stringify({ name, email, password });
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) =>
+        dispatch(setAlert(error.msg, 'danger'))
+      );
+    }
+
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
+
+// Login User
+export const login =
+  (email, password) => async (dispatch) => {
+    const body = { email, password };
 
     try {
-      const res = await axios.post(
-        'api/users',
-        body,
-        config
-      );
+      const res = await api.post('/auth', body);
 
       dispatch({
-        type: REGISTER_SUCCESS,
+        type: LOGIN_SUCCESS,
         payload: res.data,
       });
+
+      dispatch(loadUser());
     } catch (err) {
       const errors = err.response.data.errors;
 
@@ -59,8 +80,12 @@ export const register =
           dispatch(setAlert(error.msg, 'danger'))
         );
       }
+
       dispatch({
-        type: REGISTER_FAIL,
+        type: LOGIN_FAIL,
       });
     }
   };
+
+// Logout
+export const logout = () => ({ type: LOGOUT });
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,26 +8,21 @@ const {
   check,
   validationResult,
 } = require('express-validator');
+const normalize = require('normalize-url');
 
-// User Model
 const User = require('../../models/User');
 
-// @route   POST api/users
-// @desc    Register user
-// @access  Public
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 router.post(
   '/',
-  [
-    check('name', 'Name is required').not().isEmpty(),
-    check(
-      'email',
-      'Please include a valid email'
-    ).isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 }),
-  ],
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -39,21 +34,24 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      // Check if user exists
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({
-          errors: [{ msg: 'User already exists' }],
-        });
+        return res
+          .status(400)
+          .json({
+            errors: [{ msg: 'User already exists' }],
+          });
       }
 
-      // Get user gravatar
-      const avatar = gravatar.url(email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm',
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm',
+        }),
+        { forceHttps: true }
+      );
 
       user = new User({
         name,
@@ -62,13 +60,12 @@ router.post(
         password,
       });
 
-      // Encrypt password
       const salt = await bcrypt.genSalt(10);
+
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      // Return jsonwebtoken
       const payload = {
         user: {
           id: user.id,
@@ -78,9 +75,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        {
-          expiresIn: 360000,
-        },
+        { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
