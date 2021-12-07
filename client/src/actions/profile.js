a36ca86d2950893f8268edfedc5d0cec30b2a995
Ryan Ponte <ryan.ponte@gmail.com>
2021-12-07 16:14:13 -0500

    Add Education and Experience to Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,6 +6,8 @@ import {
   GET_PROFILES,
   CLEAR_PROFILE,
   PROFILE_ERROR,
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED,
 } from './types';
 
 // Get current user's profile
@@ -87,3 +89,152 @@ export const createProfile =
       });
     }
   };
+
+// Add Experience
+export const addExperience =
+  (formData, navigate) => async (dispatch) => {
+    try {
+      const res = await api.put(
+        '/profile/experience',
+        formData
+      );
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(setAlert('Experience Added', 'success'));
+
+      navigate('/dashboard');
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) =>
+          dispatch(setAlert(error.msg, 'danger'))
+        );
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
+
+// Add Education
+export const addEducation =
+  (formData, navigate) => async (dispatch) => {
+    try {
+      const res = await api.put(
+        '/profile/education',
+        formData
+      );
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(setAlert('Education Added', 'success'));
+
+      navigate('/dashboard');
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) =>
+          dispatch(setAlert(error.msg, 'danger'))
+        );
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
+
+// Delete experience
+export const deleteExperience =
+  (id) => async (dispatch) => {
+    try {
+      const res = await api.delete(
+        `/profile/experience/${id}`
+      );
+
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(setAlert('Experience Removed', 'success'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  };
+
+// Delete education
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await api.delete(
+      `/profile/education/${id}`
+    );
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Education Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+// Delete account & profile
+export const deleteAccount = () => async (dispatch) => {
+  if (
+    window.confirm('Are you sure? This can NOT be undone!')
+  ) {
+    try {
+      await api.delete('/profile');
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(
+        setAlert(
+          'Your account has been permanently deleted'
+        )
+      );
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: err.response.statusText,
+          status: err.response.status,
+        },
+      });
+    }
+  }
+};
